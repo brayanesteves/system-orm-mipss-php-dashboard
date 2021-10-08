c47e1208a9c4392ac734659ee79430f614d62387
Brayan Esteves <werneriasearch@gmail.com>
2021-10-08 11:51:17 -0400

    40 - Generating structure to add with AJAX

--- views/admin/purchases/invoices/add.php
+++ views/admin/purchases/invoices/add.php
@@ -17,69 +17,91 @@
                     </div>
                     <!-- CONTENT -->
                     <div class="row">
+                        <form action="<?php url("purchaseinvoices/add"); ?>" method="POST" role="form">
+                            <div class="col-md-6"> 
 
-                        <div class="col-md-6"> 
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title"></h3>                                    
-                                </div>
-                                <div class="panel-body">
-                                    <form action="<?php url("purchaseinvoices/add"); ?>" method="POST" role="form">
-                                        <legend>Purchase invoices data</legend>
-                                        <?php if(isset($purchaseinvoices)): ?>
-                                            <input type="hidden" name="Rfrnc" value="<?php echo $purchaseinvoices->Rfrnc; ?>">
-                                        <?php endif; ?>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h3 class="panel-title"></h3>                                    
+                                    </div>
+                                    <div class="panel-body">
                                         
-                                        <div class="form-group">
-                                            <label for="InvcNmbr">Invoice number</label>
-                                            <input type="text" name="InvcNmbr" id="InvcNmbr" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->InvcNmbr : ''; ?>" placeholder="Enter invoice number..." />
-                                        </div>
+                                            <legend>Purchase invoices data</legend>
+                                            <?php if(isset($purchaseinvoices)): ?>
+                                                <input type="hidden" name="Rfrnc" value="<?php echo $purchaseinvoices->Rfrnc; ?>">
+                                            <?php endif; ?>
+                                            
+                                            <div class="form-group">
+                                                <label for="InvcNmbr">Invoice number</label>
+                                                <input type="text" name="InvcNmbr" id="InvcNmbr" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->InvcNmbr : ''; ?>" placeholder="Enter invoice number..." />
+                                            </div>
 
-                                        <div class="form-group">
-                                            <label for="CntrlNmbr">Control number</label>
-                                            <input type="text" name="CntrlNmbr" id="CntrlNmbr" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->CntrlNmbr : ''; ?>" placeholder="Enter control number..." />
-                                        </div>
+                                            <div class="form-group">
+                                                <label for="CntrlNmbr">Control number</label>
+                                                <input type="text" name="CntrlNmbr" id="CntrlNmbr" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->CntrlNmbr : ''; ?>" placeholder="Enter control number..." />
+                                            </div>
 
-                                        <div class="form-group">
-                                            <label for="Dy">Day</label>
-                                            <input type="text" name="Dy" id="Dy" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Dy : ''; ?>" placeholder="Enter day..." />
-                                        </div>
+                                            <div class="form-group">
+                                                <label for="Dy">Day</label>
+                                                <input type="text" name="Dy" id="Dy" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Dy : ''; ?>" placeholder="Enter day..." />
+                                            </div>
 
-                                        <div class="form-group">
-                                            <label for="Mnth">Month</label>
-                                            <input type="text" name="Mnth" id="Mnth" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Mnth : ''; ?>" placeholder="Enter month..." />
-                                        </div>
+                                            <div class="form-group">
+                                                <label for="Mnth">Month</label>
+                                                <input type="text" name="Mnth" id="Mnth" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Mnth : ''; ?>" placeholder="Enter month..." />
+                                            </div>
 
-                                        <div class="form-group">
-                                            <label for="Yr">Year</label>
-                                            <input type="text" name="Yr" id="Yr" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Yr : ''; ?>" placeholder="Enter year..." />
-                                        </div>                                        
+                                            <div class="form-group">
+                                                <label for="Yr">Year</label>
+                                                <input type="text" name="Yr" id="Yr" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Yr : ''; ?>" placeholder="Enter year..." />
+                                            </div>                                        
 
-                                        <div class="form-group">
-                                            <label for="Plc">Place</label>
-                                            <input type="text" name="Plc" id="Plc" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Plc : ''; ?>" placeholder="Enter place..." />
-                                        </div>
+                                            <div class="form-group">
+                                                <label for="Plc">Place</label>
+                                                <input type="text" name="Plc" id="Plc" class="form-control" value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Plc : ''; ?>" placeholder="Enter place..." />
+                                            </div>
 
-                                        <div class="form-group">
-                                            <label for="Rfrnc_Prsn">Name person</label>
-                                            <select name="Rfrnc_Prsn" id="Rfrnc_Prsn" class="form-control">
-                                                <option value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Rfrnc_Prsn : ''; ?>" disabled><?php echo isset($purchaseinvoices) ? $purchaseinvoices->Rfrnc_Prsn : ''; ?></option>
-                                                <option value="1">1</option>
-                                                <option value="2">2</option>
-                                                <option value="3">3</option>
-                                                <option value="4">4</option>
-                                                <option value="5">5</option>
-                                            </select>
-                                        </div>
+                                            <div class="form-group">
+                                                <label for="Rfrnc_Prsn">Name person</label>
+                                                <select name="Rfrnc_Prsn" id="Rfrnc_Prsn" class="form-control">
+                                                    <option value="<?php echo isset($purchaseinvoices) ? $purchaseinvoices->Rfrnc_Prsn : ''; ?>" disabled><?php echo isset($purchaseinvoices) ? $purchaseinvoices->Rfrnc_Prsn : ''; ?></option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                    <option value="4">4</option>
+                                                    <option value="5">5</option>
+                                                </select>
+                                            </div>
 
-                                        <button type="submit" class="btn btn-primary"><?php echo isset($purchaseinvoices) ? 'Update' : 'Add'; ?></button>
-                                    </form>
+                                            <!--<button type="submit" class="btn btn-primary"><?php echo isset($purchaseinvoices) ? 'Update' : 'Add'; ?></button>-->
+                                        
+                                    </div>
+                                </div>
+                            </div> 
+
+                            <div class="col-md-12">
+                                <div class="panel panel-default">
+                                    <div class="panel-body">
+                                        <button type="button" class="btn btn-sm btn-primary">Add purchased products</button>
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Qntty</th>
+                                                    <th>Unite Price: Provider</th>
+                                                    <th>Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            </tbody>
+                                        </table>
+
+                                    </div>
                                 </div>
                             </div>
 
-                        </div> 
-
+                        </form>
                     </div>
                     <!-- .CONTENT -->
                 </div>
--- views/admininclude/footer.php
+++ views/admininclude/footer.php
@@ -3,4 +3,6 @@
     <script src="<?php assets("libs/metismenu/version/1.1.3/js/metisMenu.min.js"); ?>"></script>
     <script src="<?php assets("libs/startmin/version/0.0.1/js/startmin.js"); ?>"></script>
     <script src="<?php assets("libs/jquery/plugin/jquery-confirm/version/3.3.4/dist/jquery-confirm.min.js"); ?>"></script>
+    <script src="<?php assets("libs/angularjs/version/1.8.2/js/angular.min.js"); ?>"></script>
+    <script src="<?php assets("libs/angularjs/version/1.8.2/js/controllers/PurchaseController.js"); ?>"></script>
     <script src="<?php assets("js/main.js"); ?>"></script>
